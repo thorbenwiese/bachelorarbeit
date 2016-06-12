ed5be1b6be5f87853021985d7a76156d5674d076
Thorben Wiese <wiese@Thorbens-MBP.fritz.box>
2016-06-12 14:55:39 +0200

    new test

--- test.py
+++ test.py
@@ -1,74 +1,132 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#import alignment2
+import alignment2
 import neuer_versuch
+import argparse
 
-""" Testfälle:
+def test_random_sequences(amount,random_length,error_rate,alphabet,delta,verbose):
 
--v -rebuildonly --delta
-random_sequences(amount, random_length, error_rate, alphabet)
-random_sequences(100,50,0.15,"acgt")
-random_sequences(10,200,0.30,"acgt")
-random_sequences(30,100,0.7,"acgt")
-random_sequences(10,50,0.15,"ac")
-
-"""
-
-def test_random_sequences(amount,random_length,error_rate,alphabet,delta):
 	rseqs = neuer_versuch.random_sequences(amount,random_length,error_rate,alphabet)
 	tpa = neuer_versuch.TracePointAlignment(rseqs[0],rseqs[1],delta)
 	for i in range(0,amount*2,2):
             aln = tpa.calculate_alignment(rseqs[i],rseqs[i+1])
-            #print aln.pretty_print_alignment(aln.seq1, aln.seq2)
-            #print "Score:", aln.score
-            #print ""
-            score = aln.score
-            alignment = neuer_versuch.TracePointAlignment(rseqs[i],rseqs[i+1],delta,score)
-            alignment.calculate_intervals(aln)
+            alignment = neuer_versuch.TracePointAlignment(rseqs[i],rseqs[i+1],delta,aln.score)
+            alignment.calculate_intervals(aln,verbose)
 
     	return "OK"
 
-def test_without_cigar(seq1,seq2,delta):
+def test_without_cigar(seq1,seq2,delta,verbose):
+
 	aln = neuer_versuch.TracePointAlignment(seq1, seq2, delta)
 	test = aln.calculate_alignment(seq1,seq2)
-	print ""
-	print "ALIGNMENT\n", aln.pretty_print_alignment(test.seq1,test.seq2)
-	tp = aln.calculate_intervals(aln,True)
-	print "TP",tp.tp
+	tp = test.calculate_intervals(test,verbose)
 
 	return "OK"
 
-def main():
-	#tpa = neuer_versuch.TracePointAlignment(seq1,seq2,15)
-	#print tpa.pretty_print_alignment(seq1,seq2)
-	print "Testing random_sequences 1/4...", test_random_sequences(100,50,0.15,"acgt",5)
-	print "Testing random_sequences 2/4...", test_random_sequences(10,200,0.30,"acgt",20)
-	print "Testing random_sequences 3/4...", test_random_sequences(30,100,0.10,"acgt",15)
-	print "Testing random_sequences 4/4...", test_random_sequences(10,50,0.15,"ac",5)
+def test_with_cigar(seq1,seq2,cigar,delta,verbose):
+
+	alignment = neuer_versuch.TracePointAlignment(seq1,seq2,delta)
+	alignment1 = alignment.cigar_to_alignment(alignment.seq1, alignment.seq2, cigar)
+	tp = alignment1.calculate_intervals(alignment1,verbose)
+
+	return "OK"
+
+def test_random(verbose):
+
+	print "Testing random_sequences 1/4...", test_random_sequences(100,50,0.15,"acgt",5,verbose)
+	print "Testing random_sequences 2/4...", test_random_sequences(10,200,0.30,"acgt",20,verbose)
+	print "Testing random_sequences 3/4...", test_random_sequences(30,100,0.10,"acgt",15,verbose)
+	print "Testing random_sequences 4/4...", test_random_sequences(10,50,0.15,"ac",5,verbose)
 	print ""
+
+def test_no_cigar(verbose):
 
 	seq1 = "gagcatgttgcctggtcctttgctaggtactgtagaga"
 	seq2 = "gaccaagtaggcgtggaccttgctcggtctgtaagaga"
-	print "Testing sequences without CIGAR 1/3...", test_without_cigar(seq1,seq2,15)
+	print "Testing sequences without CIGAR 1/3...", test_without_cigar(seq1,seq2,15,verbose)
 
 	seq1 = "acgtgtggc"
 	seq2 = "aaacgggcacgccgtggcccct"
-	#print "Testing sequences without CIGAR 2/3...", test_without_cigar(seq1,seq2,10)
+	print "Testing sequences without CIGAR 2/3...", test_without_cigar(seq1,seq2,10,verbose)
 
 	seq1 = "acggacgttgacagtgtgacgtacgagacgtgtttgacagtgaccaagaatgttagag"
 	seq2 = "aggctcggacgtacgagacgtgtttggctcgagagc"
-	print "Testing sequences without CIGAR 3/3...", test_without_cigar(seq1,seq2,15)
+	print "Testing sequences without CIGAR 3/3...", test_without_cigar(seq1,seq2,15,verbose)
 	print ""
 
-	"""
+def test_cigar(verbose):
+
+	seq1 = "gagcatgttgcctggtcctttgctaggtactgtagaga" 
+	seq2 = "gaccaagtaggcgtggaccttgctcggtctgtaagaga" 
+	cigar = "5M5M1I3M5M1D9M1D5M1I2M2M"
+	print "Testing sequences with CIGAR 1/4...", test_with_cigar(seq1,seq2,cigar,15,verbose)
+	
 	seq1 = "acccccggtggct"
 	seq2 = "acgcaccgtcgcg"
 	cigar = "13M"
+	print "Testing sequences with CIGAR 2/4...", test_with_cigar(seq1,seq2,cigar,3,verbose)
 
-	print "Testing sequences with CIGAR 1/4...", 
-	print "Testing sequences with CIGAR 1/4...", 
-	print "Testing sequences with CIGAR 1/4...", 
-	print "Testing sequences with CIGAR 1/4...", """
+	seq1 = "actgaactgact"
+	seq2 = "actagaatggct"
+	cigar = "3M1I3M1D5M"
+	print "Testing sequences with CIGAR 3/4...", test_with_cigar(seq1,seq2,cigar,3,verbose)
+
+	seq1 = "gtgtcgcccgtctagcatacgc"
+	seq2 = "gggtgtaaccgactaggggg"
+	cigar = "11M1D10M"
+	print "Testing sequences with CIGAR 4/4...", test_with_cigar(seq1,seq2,cigar,3,verbose)
+
+def test_bam(verbose):
+
+	return True
+
+def test_sam(verbose):
+
+	return True
+
+def test_rebuild(verbose):
+
+	return True
+
+def main():
+
+	# TODO bisher nur Positiv-Tests
+	# TODO rebuild_intervals einfügen
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-a", "--all", help="Alle Tests durchführen", action="store_true")
+	parser.add_argument("-n", "--nocigar", help="Teste Sequenzen ohne CIGAR-String", action="store_true")
+	parser.add_argument("-c", "--cigar", help="Teste Sequenzen mit CIGAR-String", action="store_true")
+	parser.add_argument("-b", "--bam", help="Test mit BAM-File", action="store_true")
+	parser.add_argument("-s", "--sam", help="Test mit SAM-File", action="store_true")
+	parser.add_argument("-r", "--random", help="Test mit zufälligen Sequenzen", action="store_true")
+	parser.add_argument("-v", "--verbose", help="Ausführlicher Output", action="store_true")
+	args = parser.parse_args()
+
+	verbose = args.verbose
+
+	if args.all:
+		test_random(verbose)
+		test_no_cigar(verbose)
+		test_cigar(verbose)
+		test_bam(verbose)
+		test_sam(verbose)
+		test_rebuild(verbose)
+	elif args.nocigar:
+		test_no_cigar(verbose)
+	elif args.cigar:
+		test_cigar(verbose)
+	elif args.random:
+		test_random(verbose)
+	elif args.bam:
+		test_bam(verbose)
+	elif args.sam:
+		test_sam(verbose)
+	else:
+		sys.stderr.write("Falsche Eingabe der Argumente!")
+		sys.exit(1);
+
+
 if __name__ == "__main__":
     main()
